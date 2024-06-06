7f412c4645823f9ef5ff449e5962550e03a3e3e2
Logon Aluno <logonrmlocal@fiap.com.br>
2024-06-06 12:22:25 -0300

    arrumei pra cll

--- calimind/src/Componentes/Footer/Footer.jsx
+++ calimind/src/Componentes/Footer/Footer.jsx
@@ -1,23 +1,23 @@
 function Footer() {
     return (
       <footer>
-      <div class="footer-div-perguntas">
-            <div class="footer-alilhar-colum">
+      <div className="footer-div-perguntas">
+            <div className="footer-alilhar-colum">
                 <h1>Tem Alguma Pergunta?</h1>
                 <p>Escreva sua preocupação para nós e nosso especialista entrará em contato com você.</p>
             </div>
 
-            <button class="button-white">Entre Em Contato</button>
+            <button className="button-white">Entre Em Contato</button>
         </div>
 
-        <div class="footer-div">
-            <div class="footer-alilhar-colum">
+        <div className="footer-div">
+            <div className="footer-alilhar-colum">
                 <img src="../public/Logo 2.svg" alt="logo"/>
                 <p>blablabla</p>
             </div>
 
-            <div class="footer-div-contatos">
-                <div class="footer-alilhar-colum">
+            <div className="footer-div-contatos">
+                <div className="footer-alilhar-colum">
                     <h2>Links</h2>
                     <a href="#">Home</a>
                     <a href="/Sobre">Sobre Nos</a>
@@ -26,9 +26,9 @@ function Footer() {
                     <a href="/SingIn">Sing In</a>
                 </div>
 
-                <div class="footer-alilhar-colum">
+                <div className="footer-alilhar-colum">
                     <h2>Entre Em Contato</h2>
-                    <div class="footer-icones">
+                    <div className="footer-icones">
                         <a href=""><img src="../public/redesSociais/instagram.svg" alt="instagram"/></a>
                         <a href=""><img src="../public/redesSociais/github.svg" alt="github"/></a>
                         <a href=""><img src="../public/redesSociais/whatsapp.svg" alt="whatsapp"/></a>
